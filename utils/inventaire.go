b80a82d98ae85f65ccb79e9cdf33f45476f107c5
nael05 <nael.morellon@ynov.com>
2025-09-16 10:06:21 +0200

    red

--- utils/inventaire.go
+++ utils/inventaire.go
@@ -9,47 +9,83 @@ type potion struct {
 }
 
 func (p *potion) achat() {
+	fmt.Println("")
 	fmt.Println("Achète tes potions ici !!")
 	fmt.Println("")
-	fmt.Println("1 : Potion rouge, augmente de  15 PV 150 Pièces")
-	fmt.Println("2 : Potion bleu, inflige 15 PV supplémentaires en cas de victoire de la manche suivante200    200 Pièces")
-	fmt.Println("3 : Potion poison, empoisonne l'ennemie de 5 PV pendant 3 tours    250 Pièces")
-	fmt.Println("4 : Potion vengeance, renvoie les dégats reçus en cas de défaite de la manche    400 Pièces")
-	fmt.Println("5 : Potion voyance, si le joueur devine le signe du joueur adverse, il lui infligera 50 PV    500 Pièces")
-	
-	Potion_rouge := potion{1, 150, 15}
-	Potion_bleu := potion{2, 200, 15}
-	Potion_poison := potion{3, 250, 5}
-	Potion_vengeance := potion{4, 400, 0}
-	Potion_voyance := potion{5, 500, 50}
+	fmt.Println("1 : Potion rouge, augmente de 15 PV 150 Pièces")
+	fmt.Println("2 : Potion bleu, inflige 15 PV supplémentaires en cas de victoire de la manche suivante 200 Pièces")
+	fmt.Println("3 : Potion poison, empoisonne l'ennemie de 5 PV pendant 3 tours 250 Pièces")
+	fmt.Println("4 : Potion vengeance, renvoie les dégâts reçus en cas de défaite de la manche 400 Pièces")
+	fmt.Println("5 : Potion voyance, si le joueur devine le signe du joueur adverse, il lui infligera 50 PV 500 Pièces")
 
-	var choix int
-	fmt.Print("Choisissez une potion (1-5) : ")
-	fmt.Scanln(&choix)
+	Potion_rouge := potion{1, 15, 150}
+	Potion_bleu := potion{2, 15, 200}
+	Potion_poison := potion{3, 5, 250}
+	Potion_vengeance := potion{4, 0, 400}
+	Potion_voyance := potion{5, 50, 500}
 
-	inventaire := []string()
+	inventaire := []string{}
+	solde := 1000
+	fmt.Println("")
+	fmt.Println("Solde :", solde)
 
-	switch choix {
-	case 1:
-		fmt.Println("Vous avez choisi :", Potion_rouge)
-		inventaire = inventaire + "Potion rouge"
-	case 2:
-		fmt.Println("Vous avez choisi :", Potion_bleu)
-		inventaire = inventaire + "Potion bleu"
-	case 3:
-		fmt.Println("Vous avez choisi :", Potion_poison)
-		inventaire = inventaire + "Potion poison"
-	case 4:
-		fmt.Println("Vous avez choisi :", Potion_vengeance)
-		inventaire = inventaire + "Potion vengeance"
-	case 5:
-		fmt.Println("Vous avez choisi :", Potion_voyance)
-		inventaire = inventaire + "Potion voyance"
-	default:
-		fmt.Println("Choix invalide !")
+	for solde >= 150 {
+		var choix int
+		fmt.Print("Choisissez une potion (1-5, 0 pour arrêter) : ")
+		fmt.Scanln(&choix)
+
+		switch choix {
+		case 1:
+			fmt.Println("")
+			fmt.Println("Vous avez choisi :", "Potion_rouge")
+			inventaire = append(inventaire, "Potion rouge")
+			solde -= Potion_rouge.prix
+			fmt.Println("")
+			fmt.Println("Solde restant :", solde)
+		case 2:
+			fmt.Println("")
+			fmt.Println("Vous avez choisi :", "Potion_bleu")
+			inventaire = append(inventaire, "Potion bleu")
+			solde -= Potion_bleu.prix
+			fmt.Println("")
+			fmt.Println("Solde restant :", solde)
+		case 3:
+			fmt.Println("")
+			fmt.Println("Vous avez choisi :", "Potion_poison")
+			inventaire = append(inventaire, "Potion poison")
+			solde -= Potion_poison.prix
+			fmt.Println("")
+			fmt.Println("Solde restant :", solde)
+		case 4:
+			fmt.Println("")
+			fmt.Println("Vous avez choisi :", "Potion_vengeance")
+			inventaire = append(inventaire, "Potion vengeance")
+			solde -= Potion_vengeance.prix
+			fmt.Println("")
+			fmt.Println("Solde restant :", solde)
+		case 5:
+			fmt.Println("")
+			fmt.Println("Vous avez choisi :", "Potion_voyance")
+			inventaire = append(inventaire, "Potion voyance")
+			solde -= Potion_voyance.prix
+			fmt.Println("")
+			fmt.Println("Solde restant :", solde)
+		case 0:
+			fmt.Println("")
+			fmt.Println("Achat terminé.")
+			solde = 0
+			fmt.Println("")
+		default:
+			fmt.Println("")
+			fmt.Println("Choix invalide !")
+			fmt.Println("")
+		}
 	}
+
+	fmt.Println("Inventaire final :", inventaire)
 }
 
-	
-
+func main() {
+	var p potion
+	p.achat()
 }
